785e8ca3e220504013e0981a2e48d4366f5758be
Artur-17 <arturandrade40@hotmail.com>
2021-03-22 19:25:08 -0300

    componentização

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { MoviesProvider } from './MoviesContext';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+export function App() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+    
+    <MoviesProvider>
+      <SideBar />
+      <Content />
+    </MoviesProvider>
+    
+      
+    </div>
+  )
+}
